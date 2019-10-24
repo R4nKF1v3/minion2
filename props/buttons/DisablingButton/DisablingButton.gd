bb13231c285abacfbeec8be88c6742a396df14cf
R4NK <nahumoscatelli@gmail.com>
2019-10-23 21:26:49 -0300

    Agregado DisableButton

--- props/buttons/DisablingButton/DisablingButton.gd
+++ props/buttons/DisablingButton/DisablingButton.gd
@@ -6,3 +6,6 @@ func _on_Area2D_body_entered(body):
 	if holders.empty():
 		signals.emit_signal("disabled", elements_ids)
 	holders.push_back(body)
+
+func _on_Area2D_body_exited(body):
+	holders.remove(holders.find(body))
